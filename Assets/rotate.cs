5cefc57524316beed30bf778fb2c64d0c9607508
Piyush Mehta <me@piyushmehta.com>
2019-03-16 14:37:10 +0530

    Rotatable components

--- Assets/rotate.cs
+++ Assets/rotate.cs
@@ -1,18 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class rotate : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class Rotate : MonoBehaviour {
+ 
+ // Update is called once per frame
+ void Update () {
+  transform.Rotate(0,20*Time.deltaTime,0);
+ }
 }
